8da331396b4adf0f0f010dd49783473344cb14bc
yyang325 <frank.young325@gmail.com>
2016-05-07 13:50:50 -0400

    learn how to export module.

---
+++ 02-node-javascript/app.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var module1 = require('./module1');
+var module2 = require('./module2');
+
+function onRequest(resquest, response){
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello World\n');
+    response.write(module2.myVariable);
+    module2.myFunction();
+    response.end();
+}
+
+http.createServer(function (resquest, response){
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello World\n');
+    response.write(module2.myVariable);
+    module2.myFunction();
+    response.end();
+}).listen(8000);
+
+console.log('server starting at localhost:8000');
